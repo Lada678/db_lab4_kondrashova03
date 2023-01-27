385b1f54ee01ad60a62f56104766eee12097f4b3
LadaKondrashova <55762041+Lada678@users.noreply.github.com>
2023-01-27 09:42:50 +0200

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,1 +1,20 @@
 
+CREATE TABLE accident_audit(
+	id SERIAL PRIMARY KEY,
+	updated TIMESTAMP,
+	user_name varchar(50) NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION accident1_audit() RETURNS trigger 
+LANGUAGE 'plpgsql'
+AS $$
+BEGIN
+ 	INSERT INTO accident_audit(updated, user_name)
+		VALUES(NOW(),  current_timestamp, user);
+	RETURN NULL;
+END;
+$$ ;
+
+CREATE TRIGGER accident1_update_details 
+AFTER UPDATE ON accident
+FOR EACH ROW EXECUTE FUNCTION accident1_audit();
